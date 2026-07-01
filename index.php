edfd769133f49522cf45df140af04f7b3b92c50b
John Sheer <meskojohn@gmail.com>
2026-07-01 03:10:24 +0000

    availability: margin-top -20px

--- index.php
+++ index.php
@@ -206,7 +206,7 @@ $stack = [
             cursor: pointer;
             text-decoration: none;
             transition: background 0.2s ease, box-shadow 0.2s ease;
-            margin-top: -10px;
+            margin-top: -20px;
         }
         .availability:hover {
             background: rgba(52, 184, 138, 0.2);
